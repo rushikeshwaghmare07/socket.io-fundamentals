73798f6c6f9524734b6fa841d600abeea7c4310a
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-10-16 22:20:28 +0530

    Serving HTML file

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import dotenv from "dotenv"
 import http from "http";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { Server } from "socket.io";
 
+dotenv.config({
+  path: "./.env",
+});
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8001;
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serving HTML file
+const _dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (req, res) => res.sendFile(join(_dirname, "index.html")));
 
 app.listen(PORT, () => {
   console.log(`Server is running at port : ${PORT}`);
